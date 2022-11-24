f646f965d3da8593cbb66a967724a782ed04a1e4
CarlosGuarneros <113079549+CarlosGuarneros@users.noreply.github.com>
2022-11-23 22:22:27 -0600

    Add files via upload

---
+++ javascript parte 2/ejemplo16/js/main.js
@@ -0,0 +1,4 @@
+const albumview = document.querySelector('#album-uno')
+const almbum2 = document.querySelector('#album-dos')
+const album1 = new Album(albumview, PHOTO_LIST)
+const album2 = new Album(almbum2, REGALOS_ENLACES)
